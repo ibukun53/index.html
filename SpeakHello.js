69657a0ac5a45695c0fae223fc0b47dc367736af
debbyblessing <debbyblessing1234@gmail.com>
2021-05-24 09:29:21 +0100

    Enfold the content of SpeakHello.js inside of an IIFE

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,30 +1,34 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-
-
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
 (function (window){
-var helloSpeaker ="speak";
-helloSpeaker.name = "Yaakov", "John","Jen", "Jason", "Paul", "Frank", "Larry","Paula", "Laura", "Jim";
+var helloSpeaker ={};
+helloSpeaker.Speak ="Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry";
+ helloSpeaker.name= function(){
+   console.log(helloSpeaker + " " + name);
+}
+   // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1.
+
+function helloSpeaker(){
+	var helloSpeaker = function speak(name){
+	console.log(speakWord + " " + name);
+		}("Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry" );
+	};  
+// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+// STEP 4: Rewrite the 'speak' function such that it is attached to the
+// helloSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2.
 var speakWord = "Hello";
 helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
- window.helloSpeaker = helloSpeaker;
- console.log("Hello "+ window);
-})("Jason",);
-
-// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-
-
-// STEP 4: Rewrite the 'speak' function such that it is attached to the
-// helloSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+// {'helloSpeaker' on the global scope as well.
+// See Lecture 52, part 2.
+(function (helloSpeaker) {
+  console.log("Hello "+  helloSpeaker);
+})("Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry" );
+
+// (Note, Step 6 will be done in the SpeakGoodBye.js file.
 // xxxx.xxxx = helloSpeaker;
--- script.js
+++ script.js
@@ -35,31 +35,19 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function (name) {
   console.log("Hello "+ name);
-})("John");
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry",
- "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
-
-})();
-
+})("John", "Jen", "Jason");
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry",
+ "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
+   // Use the string object's 'charAt' function. Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
@@ -71,3 +59,9 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names["J"]);
+  } else {
+    helloSpeaker.speak(names['j']);
+  }
+}
